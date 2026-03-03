e7de88d0009576f964a0726f14c0343da4fba5cc
John Schumacher <jschumacher@sfsu.edu>
2026-03-02 21:50:51 -0800

    03022026 21:50 commit

--- containment/js/containment.js
+++ containment/js/containment.js
@@ -1,34 +1,82 @@
+// 02242026 - built out containment with keypress
+// 02262026 - adding time as a factor
+
 let offset=0;
+let offsetY=0;
+let startTime;
+let timeLimit = 10000;   //milisecs
+let timerStarted = false;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER);
-    // MEASURES FROM CENTER OF SHAPE
+    rectMode(CENTER);     // MEASURES FROM CENTER OF SHAPE
+    // startTime = millis();
 }
 
 function draw(){
     background(0);
 
+    if (timerStarted) {
+    let elapsed = millis() - startTime;
+    let remaining =  floor((timeLimit - elapsed)/1000);
+    timerStarted = true;
+    textSize(32);
+    fill(255);
+    text("Time: " + remaining , width/2, 50);
+    
+    if(elapsed>timeLimit){
+        offset=0;
+        offsetY=-0;
+        startTime = millis();
+    }
+        
+        
+    if(remaining <= 0) {
+    remaining = 0;
+    timerStarted = false;
+    }
+
+   
+
+    
+        
+    
+
     push();
-    translate(offset, offset); 
+    translate(offset, offsetY); 
     // changes, the x,y
     fill(100, 200, 100);
-    rect(width/3, height/2, 100, 100);  /*can apply offset to other parameters*/
+    rect(width *2/3, height/2, 100, 100);      /*can apply offset to other parameters*/
     pop();
     
-    
-    rect(width *2/3, height/2, 100, 100);
-    
+    // static rect
+    rect(width/3, height/2, 100, 100); 
 }
+}
+        
+function keyPressed(){ 
+    if(!timerStarted){
+    timerStarted = true;
+    offset += random(-50, 50);
+    offsetY += random(-75, 75);
+     } else {
+    if(elapsed>timeLimit){
+        offset=0;
+        offsetY=-0;
+        startTime = millis();
+        }
+    }
+}
+    
+    
 
-function keyPressed(){
-offset+= 20;
+// offset+= 20;
+
 // or could be random offset += random(-100,100);
 // or add constraint offset = contrain(offset, -200,200);
 
 // take current value of offset and each time key is pressed, adds 20 to value, and save to new value
-}
 
 function windowResized(){
-    resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(windowWidth, windowHeight);
 }
